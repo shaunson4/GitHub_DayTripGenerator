4ce2453d15cd029822d5498c07f57a6fa574a8bc
shaunson4 <sgray4106@gmail.com>
2022-03-30 09:15:34 -0500

    Installed while loops to the category functions to help eliminate previous random selections to minimize redundancy.

--- Day Trip Generator Project.py
+++ Day Trip Generator Project.py
@@ -11,41 +11,48 @@ entertainment = ['dancing', 'theatre', 'play', 'opera']
 
 
 def select_dest():
-    dest = random.choice(destinations)
-    dest_choice = input(f'We have selected {dest} for your destination! Does this sound good? Enter y/n:')
-    if dest_choice == 'y': 
-        print('Awesome! Glad that you approve of this destination. Let\'s continue')
-        return dest
-    else:
-        print('Oh, my apologies, you don\'t approve of this destination. No problem at all, we can try something else.')
-        select_dest() 
+    while True:
+        dest = random.choice(destinations)
+        dest_choice = input(f'We have selected {dest} for your destination! Does this sound good? Enter y/n:')
+        if dest_choice == 'y': 
+            print('Awesome! Glad that you approve of this destination. Let\'s continue')
+            break 
+        else:
+            print('Oh, my apologies, you don\'t approve of this destination. No problem at all, we can try something else.')
+    return dest 
 
 def select_rest():
-    eatery1 = random.choice(restuarants)
-    rest_choice = input(f'We have selected {eatery1} for your dining pleasure. Doesn\'t that sound tantilizing? Enter y/n:')
-    if rest_choice == 'y':
-        print('Awesome! Glad that you approve of this restaurant. Let\'s continue')
-        return eatery1
-    else:
-        print('Oh, my apologies, you don\'t approve of this restaurant. No problem at all, we can try another place to eat.')
-        select_rest()
+    while True: 
+        eatery1 = random.choice(restuarants)
+        rest_choice = input(f'We have selected {eatery1} for your dining pleasure. Doesn\'t that sound tantilizing? Enter y/n:')
+        if rest_choice == 'y':
+            print('Awesome! Glad that you approve of this restaurant. Let\'s continue')
+            break 
+        else:
+            print('Oh, my apologies, you don\'t approve of this restaurant. No problem at all, we can try another place to eat.')
+    return eatery1
 
 def select_transport():
-    transport1 = random.choice(mode_of_transportation) 
-    transpo_choice = input(f'We have selected {transport1} for your mode of transportation. Doesn\'t that sound relaxing? Enter y/n:')
-    if transpo_choice =='y':
-        print('Awesome! Glad that you approve of this mode of transportation. Let\'s continue')
-        return transport1
-    else:        
-        print('Oh, my apologies, you don\'t approve of this type of transportation. No problem at all, we can try different one.')
-        select_transport()
+    while True:
+        transport1 = random.choice(mode_of_transportation) 
+        transpo_choice = input(f'We have selected {transport1} for your mode of transportation. Doesn\'t that sound relaxing? Enter y/n:')
+        if transpo_choice =='y':
+            print('Awesome! Glad that you approve of this mode of transportation. Let\'s continue')
+            break 
+        else:        
+            print('Oh, my apologies, you don\'t approve of this type of transportation. No problem at all, we can try different one.')
+    return transport1
 
 def select_activity():
-    activity1 = random.choice(entertainment)
-    activity_choice = input(f'We have selected {activity1} for your entertainment. Doesn\'t that sound exciting? Enter y/n:')
-    if activity_choice == 'y':
-        print('Awesome! Glad that you approve of the entertainment chosen. Let\'s summarize')
-        return activity1
+    while True:
+        activity1 = random.choice(entertainment)
+        activity_choice = input(f'We have selected {activity1} for your entertainment. Doesn\'t that sound exciting? Enter y/n:')
+        if activity_choice == 'y':
+            print('Awesome! Glad that you approve of the entertainment chosen. Let\'s summarize')
+            break
+        else:
+            print('Oh, my apologies, you don\'t approve of this type of entertainment. No problem at all we can try a different one')
+    return activity1 
 
 def confirm_completion():
         complete = input('Congratulations! We you have approved selections in all 4 categories of Day trip and it is now complete. Would you agree to said completion: Enter y/n: ')
@@ -64,15 +71,13 @@ def main_itinerary():
     activity1 = select_activity()
 
     confirm_completion()
-    
+
     print('The trip we have generated for you is as follows: ')
 
     print(f'Trip Destination: {dest} ')
     print(f'Restaurant: {eatery1}')
     print(f'Mode of Transporation: {transport1}')
     print(f'Entertainment: {activity1}')
-
-
 
 main_itinerary()
     
